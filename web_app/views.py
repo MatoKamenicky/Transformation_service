64a556aa6a6724f9273c800138be2103706195d0
Matej Kamenický <maty.kamenicky@gmail.com>
2023-12-18 23:27:50 +0100

    django first steps

--- web_app/views.py
+++ web_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-import Transformation_service
+import Transformation_service as ts
 
 
 
 def web_transform(request):
-    template = loader.get_template('template_try.html')
-    return HttpResponse(template.render())
+    x, y, z = 123456, 789012, 100  # Example height in meters
+
+    from_epsg = 5514
+    to_epsg   = 4326
+    result = ts.transform_coordinates(x, y, z, from_epsg, to_epsg)
+    return render(request, 'result_template.html', {'result': result})
+
